0bc1a3aa8684030541f7d0e9283fa42260ead0fb
Pavel Shitikov <pashicop@gmail.com>
2022-03-10 11:02:39 +0300

    test

---
+++ main.py
@@ -0,0 +1,21 @@
+documents = [
+        {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
+        {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
+        {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
+      ]
+directories = {
+    '1': ['2207 876234', '11-2'],
+    '2': ['10006'],
+    '3': []
+}
+
+def get_comand():
+    dfgdfgfgdfgd
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # print_hi('PyCharm')
+
+
